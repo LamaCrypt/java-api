78e528e41714897e50fce8eefcb2a06d4868a7a5
LamaGuy <LamaGuy@users.noreply.github.com>
2017-03-11 01:11:51 +0100

    populated GPTLS

--- src/ch/lamacrypt/internal/network/GPTLS.java
+++ src/ch/lamacrypt/internal/network/GPTLS.java
@@ -8,6 +8,41 @@
  */
 package ch.lamacrypt.internal.network;
 
-public class GPTLS {
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
 
+/**
+ * Contains general purpose methods related to TLS
+ *
+ * @author LamaGuy
+ */
+public class GPTLS {
+        /**
+     * Sets the supported TLS version and cipher suite(s)
+     *
+     * @param socket SSLSocket to act upon
+     */
+    public static void setTLSParams(SSLSocket socket) {
+        // sets the supported TLS protocol and cipher suites
+        socket.setEnabledProtocols(new String[]{"TLSv1.2"});
+        socket.setEnabledCipherSuites(new String[]{
+            "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+            "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"});
+    }
+
+    /**
+     * Returns an SSLContext object which instantiates TLS
+     *
+     * @return SSLContext for TLS
+     * @throws java.security.NoSuchAlgorithmException
+     * @throws java.security.KeyManagementException
+     */
+    public static SSLContext getContext() throws NoSuchAlgorithmException, KeyManagementException {
+        // creates the SSLContext needed to instantiate the SSLSocket
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, null, null);
+        return sslContext;
+    }
 }
